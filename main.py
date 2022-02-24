496ad910cd880523257f064a92535c6a6c7316d1
Beacon Brigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-02-24 17:14:15 +0000

    Straightening out files

--- main.py
+++ main.py
@@ -5,7 +5,11 @@
 import pytest
 from equation_balancer.utils import reader, equality
 
+<<<<<<< HEAD
 a,b,c,d = reader("C8H18 + O2 -> CO2 + H2O")
+=======
+a,b,c,d = reader("Al + O2 -> Al2O3")
+>>>>>>> origin/main
 equality(c,d)
 
 #reader("(NH4)3PO4 + Mg(CH3COO)2 -> Mg3(PO4)2 + NH4CH3COO")
