4a774103aeec05ff21fb309c72a94dc41ac24799
Beacon Brigade <100320298+BeaconBrigade@users.noreply.github.com>
2022-02-28 01:02:47 +0000

    changed __init__ and main

--- chemBal/__init__.py
+++ chemBal/__init__.py
@@ -1,1 +1,3 @@
-print("Hello World!")
+import colorama as clr
+
+print(f"{clr.Fore.BLUE}Welcome to chemBal. Input a chemical equation to get it balanced.\nVisit https://github.com/BeaconBrigade/Chemical-Equation-Balancer for the repository.{clr.Style.RESET_ALL}")
