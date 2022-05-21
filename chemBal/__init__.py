22b57533e03638ed592aa84c6fd4b8b11d2e490f
Ryan <beaconBrigade@gmail.com>
2022-05-20 22:37:25 -0230

    Changed init to load module more user friendly

--- chemBal/__init__.py
+++ chemBal/__init__.py
@@ -1,3 +1,4 @@
 import colorama as clr
+from utils import balance
 
 print(f"{clr.Fore.BLUE}Welcome to chemBal. Input a chemical equation to get it balanced.\nVisit https://github.com/BeaconBrigade/Chemical-Equation-Balancer for the repository.{clr.Style.RESET_ALL}")
